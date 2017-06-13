11e19c0ef2c86c5fd0fa4c9615117d846d5e5bd4
Eric Petersson <ericpetersson@Erics-iMac.lan>
2017-06-13 23:47:13 +0200

    added environment variables

--- app/api.js
+++ app/api.js
@@ -1,4 +1,5 @@
-const Dota2Api = require('dota2-api');
-const da = Dota2Api.create('484ADB51C401C2EC25E043ADEF5E9554');
+import {} from 'dotenv/config';
+import Dota2Api from 'dota2-api';
+const da = Dota2Api.create(process.env.API_KEY);
 
 module.exports = da;
